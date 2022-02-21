24f12e22c6d0435082e55550b990a21f0e729075
Madhusudan Jha <madhusudanjha14071999@gmail.com>
2022-02-21 15:42:29 +0530

    Week 1 Day 3 Task 6

--- day3/task3/index.js
+++ day3/task3/index.js
@@ -1,6 +1,6 @@
 function printFib() {
     num = document.getElementById("readnum").value;
-    console.log(fib(num))
+    console.log(fibIttrative(num))
 }
 
 function fib(num) {
@@ -12,5 +12,12 @@ function fib(num) {
 
 
 function fibIttrative(num) {
-    
+    let arr = [1, 1];
+    // if (num <= arr.length) return arr[num-1]
+
+    for(let i=2; i<num; i++) {
+        arr.push(arr[i-1] + arr[i-2]);
+    }
+    // console.log(arr);
+    return arr;
 }
---
+++ day3/task6/js/main.js
@@ -0,0 +1,9 @@
+function submitContactForm() {
+    let name = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+    let phone = document.getElementById("phone").value;
+
+    console.log(name);
+    console.log(email);
+    console.log(phone);
+}
